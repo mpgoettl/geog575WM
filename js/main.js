61d8e98d0ce64b78bab4981cbe4a03a0702b7bfc
mpgoettl <mpgoettl@hotmail.com>
2021-06-20 18:17:53 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,8 +14,8 @@ $(document).ready(function() {
 		});
 		
 		var basemaps = [
-			L.tileLayer('//{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
-				attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+			L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
+				attribution: 'Map tiles by <a href="https://stamen.com">Stamen Design</a>, <a href="https://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
 				label: 'Toner Lite',				// optional label used for tooltip
 				iconURL: 'img/tonerGray.png'
 			}),
